29101f8cab784be03ec077d71a01d34a898beea3
Yu-Chia Wu <gerrywu@utexas.edu>
2017-09-28 00:59:39 -0500

    Set PORT environment variable

--- app.py
+++ app.py
@@ -8,4 +8,7 @@ def index():
     return render_template('index.html', **locals())
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # https://stackoverflow.com/questions/17260338/deploying-flask-with-heroku
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
