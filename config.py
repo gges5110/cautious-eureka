04f36ad0127a17b96a2ec2b4ca76916a7ae7ccc4
Yu-Chia Wu <gerrywu@utexas.edu>
2017-10-08 02:00:27 -0500

    Set database URI

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 class Config(object):
     DEBUG = False
     TESTING = False
@@ -6,7 +8,7 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = 'postgres://sid:sid12345@localhost:5432/flash_cards'
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = 'postgres://sid:sid12345@localhost:5432/flash_cards'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(Config):
     DEBUG = True
